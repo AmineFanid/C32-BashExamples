7d9a3e7af65b106a825a3f8e3c9c8e67ab21b3e4
AmineFanid <fan.aminee@gmail.com>
2023-05-01 10:23:32 -0400

    LES CONDITIONS-NTC

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
+echo -n "note? : "
+read note
 
 
+
+#if test ...
+#if [...]
+#if [[...]]
+
+if [[ $note -lt 60 ]]
+then
+    echo "Désolé... vous échoué"
+elif test $note -eq 60
+then
+    echo "Ouf!"
+else 
+    echo "Beau travail!"
+fi
+#ou
+if [[ $note -lt 60 ]];then
+    echo "Désolé... vous échouez";fi
+
+read -p "Entrez une lettre " lettre
+case $lettre in
+    [[:lower:]])
+        echo "La lettre est en miniscule"
+        ;;
+    *)
+        echo "Autre"
+        ;;
+esac
+
+if [[ $lettre != "a" ]]
+then
+    echo "diff de a"
+fi
 
 
 
