03a1b1ce40bc730c8eee4ce1803454286c09c7aa
AmineFanid <fan.aminee@gmail.com>
2023-05-01 09:45:04 -0400

    changement

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -3,3 +3,12 @@
 #allo
 echo "Hello" $USER
 
+#exit
+
+echo -n "Vive "
+echo "Linux"
+
+# -e permet d'interpreter les retours de ligne et etc.. (\n, \t, ...)
+echo -e "1\n2\n2"
+read -p "Quele age avez-vous?" age
+echo $age "! Vous etes jeune!"
