356e476a0e43c088294e610e60c84d90ceb59064
AmineFanid <fan.aminee@gmail.com>
2023-05-01 12:20:54 -0400

    reponse ex3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+espace(){
+    for (( i=0;i<$1;i++ ))
+    do
+        echo -n " "        
+    done
+}
+
+for ((i=0;i<30;i++))
+do
+clear
+espace $i
+if [[ $i -lt 29 ]]
+then
+    echo "@"
+else
+    echo "BOOM"
+fi
+sleep 0.1
+done
