1fc86e7ad5f898fbf1e3c17d64ae5d2480afdf6d
AmineFanid <fan.aminee@gmail.com>
2023-05-01 10:03:35 -0400

    comment faire des calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo 10+10
+#il faut mettre l'équation dans des doubles parenthese(())
+echo $((10+10))
+#si on a besoin de le mettre en memoire
+valeur=$((10+10))
+echo $valeur
+
+read -p "nb 1 : " nb1
+read -p "nb 2 : " nb2
+let resultat=$nb1*$nb2
+echo $resultat
