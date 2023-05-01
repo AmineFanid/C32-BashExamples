c5940be39fb6c769ef8255d5a05811734a017794
AmineFanid <fan.aminee@gmail.com>
2023-05-01 10:43:44 -0400

    boucles-ntc

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+    echo $i
+done
+
+# `` = execution d'une commande et mettre dans variable
+liste=`ls`
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
