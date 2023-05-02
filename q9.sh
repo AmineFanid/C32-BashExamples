4617c4183fddadc56d36a3479553a6309c7c8ae7
AmineFanid <fan.aminee@gmail.com>
2023-05-02 13:15:40 -0400

    rep q9 lab BASH

---
+++ q9.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#question 9 du lab bash
+
+boom(){
+    echo "========================"
+    echo "!! YOU WERE INFECTED !!"
+    echo "========================"
+    echo "=========.-.============"
+    echo "========(o.o)==========="
+    echo "=========|=|============"
+    echo "==========|============="
+    echo "======= __|__==========="
+    echo "SAY GOODBYE TO YOUR FILES"
+    echo -n "Bureau" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Documents" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Images" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Modèles" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Musique" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Public" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Téléchargements" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+    echo -n "Vidéos" 
+    sleep 0.3
+    echo -n "..."
+    sleep 0.3
+    echo "deleted"
+}
+
+boom
+
